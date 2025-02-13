9ff5d7512db7e2e6108d1b8ed009d4617a5086b1
luisenrique <luisenriqueuriartefranco@gmail.com>
2025-02-12 22:07:37 -0600

    Evaluador de notas segunda clase

---
+++ EvaluadorDeNotas.js
@@ -0,0 +1,14 @@
+const nota=74
+
+if(nota>=90){
+    console.log("Excelente");
+}
+else if(nota>=75 && nota <=89){
+    console.log("bien");
+}
+else if(nota>=60 && nota <=74){
+    console.log("suficiente");
+}
+else{
+    console.log("No aprueba")
+}
